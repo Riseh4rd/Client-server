7398225df2b234ab517d6385c4bfdfefd9108a40
Riseh4rd <112888563+Riseh4rd@users.noreply.github.com>
2022-09-26 14:08:36 +0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -3,40 +3,38 @@ import java.io.*;
 import java.util.*;
 
 public class Client {
-	static int port = 5003; // Порт, такой же, как у сервера
-	static String address = "127.0.0.1"; // Адрес сервера
+	static int port = 5003; 
+	static String address = "127.0.0.1"; 
 	
 	public static void main(String[] args) {
         try {
-	    // Преобразуем адрес из строки во внутреннее представление
+	    
 	    InetAddress addr = InetAddress.getByName(address);
         	
-            // Создаём сокет и подключаем его к серверу
+          
 	    System.out.println("Поключаемся к " + address + ":" + port + "...");
 	    Socket socket = new Socket(addr, port);
             
-            // Потоки ввода/вывода
-	    // BufferedReader позволяет читать вход по строкам
+           
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	    // PrintWriter позволяет использовать println
+	  
             PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
-            // Создаем объект для чтения строк с клавиатуры
+           
             Scanner scan = new Scanner(System.in, "UTF-8");
 
-            // Читаем данные с клавиатуры и отправляем серверу
-	    // Запрашиваем строку
+            
 	    System.out.print("[Запрос]:");
 	    String line = scan.nextLine();
 
-	    // Отправляем строку серверу
+	   
 	    out.println(line);
-	    out.flush(); 	// принудительная отправка
+	    out.flush(); 	
             
-	    // Получаем ответ
-	    line = in.readLine(); // ждем пока сервер отошлет строку текста.
+	   
+	    line = in.readLine(); 
 	    System.out.println("[Ответ]:" + line);
 
-	    // Закрываем соединение
+	  
 	    socket.close();
         } catch (IOException x) {
             System.out.println("Ошибка ввода/вывода");
